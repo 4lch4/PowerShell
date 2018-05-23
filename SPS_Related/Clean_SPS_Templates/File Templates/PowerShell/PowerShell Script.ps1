c6ed90bf610a535a785f1e6e15a7afe7dd0c4066
Devin Leaman <dleaman212@gmail.com>
2018-05-22 23:51:50 -0500

    Moved all PowerShell Studio stuff to its own folder

---
+++ SPS_Related/Clean_SPS_Templates/File Templates/PowerShell/PowerShell Script.ps1
@@ -0,0 +1,15 @@
+﻿#	===========================================================================
+#	Created on:   %Date% @ %Time%
+#	Created by:   %Username%
+#	Organization: %Company%
+#	Filename:     %FileName%
+#	===========================================================================
+
+<#
+.DESCRIPTION
+	A description of the file.
+
+.EXAMPLE
+	An example of the file.
+#>
+
