64c2fc4ebf29ce509901a19b538e00d50a9c1692
Devin Leaman <dleaman212@gmail.com>
2018-05-28 00:08:24 -0500

    Updated format
    I personally prefer 2 spaces for tabs at this point, not too sure why I used 4 when I first committed this.

--- Scripts/New-NodeModule.ps1
+++ Scripts/New-NodeModule.ps1
@@ -57,132 +57,131 @@ what this is, see http://bit.ly/poshprofiles):
 . $ENV:HOMEDRIVE\$ENV:HOMEPATH\Documents\WindowsPowerShell\New-NodeModule.ps1
 #>
 function New-NodeModule {
-    [CmdletBinding()]
-    [Alias('nodemod', 'new-nodemod', 'newmod', 'newmodule', 'newnpm')]
-    param
-    (
-        [Parameter(Mandatory = $true,
-                   Position = 1,
-                   HelpMessage = 'The name of the module to create.')]
-        [ValidateScript({
-                if ($_ -match '^(?:@[a-z0-9-~][a-z0-9-._~]*/)?[a-z0-9-~][a-z0-9-._~]*$') {
-                    return $true
-                } else {
-                    throw "Your module name must match the following pattern: ^(?:@[a-z0-9-~][a-z0-9-._~]*/)?[a-z0-9-~][a-z0-9-._~]*`$`nSee https://docs.npmjs.com/files/package.json#name for more info."
-                }
-            })]
-        [Alias('Name', 'Module')]
-        [String]$ModuleName,
-        
-        [Parameter(Mandatory = $false,
-                   Position = 2,
-                   HelpMessage = 'The path where the module is created.')]
-        [ValidateScript({
-                if (Test-Path $_) {
-                    return $true
-                } else {
-                    throw 'The provided path does not exist.'
-                }
-            })]
-        [Alias('Path')]
-        [System.IO.FileInfo]$ModulePath = 'D:\Development\Projects\NodeJS_Packages',
-        
-        [Parameter(Mandatory = $false,
-                   Position = 3,
-                   HelpMessage = 'Do you wish to open this module in VSCode after creation?')]
-        [Alias('Open')]
-        [bool]$OpenInVSCode = $true
-    )
-    
-    $AuthorInfo = Get-AuthorInfo
-    $ScriptsInfo = Get-ScriptsInfo
-    $DevDependencies = Get-DevDependencies
-    $PackageInfo = Get-PackageInfo -ModuleName $ModuleName
-    
-    $FullModulePath = Join-Path -Path $ModulePath -ChildPath $ModuleName
-    $PackageJson = Join-Path -Path $FullModulePath -ChildPath 'package.json'
-    
-    if (Test-Path -Path $FullModulePath) {
-        # There is a folder at the provided path with the same name as the $ModuleName
-        throw 'A folder with the same name exists at the provided path. Please choose a different module name or path.'
-    } else {
-        # Create directory for the module
-        Write-Debug 'Creating module directory...'
-        New-Item -Path $FullModulePath -ItemType Directory | Out-Null
-        
-        # Create the new package.json file
-        Write-Debug 'Creating modules package.json...'
-        New-Item -Path $PackageJson -ItemType File | Out-Null
-        
-        # Create the main index.js for most code logic
-        Write-Debug 'Creating modules index.js...'
-        New-Item -Path $FullModulePath\index.js -ItemType File | Out-Null
-        
-        # Create the LICENSE file that will contain the MIT License
-        Write-Debug 'Creating modules empty LICENSE file...'
-        New-Item -Path $FullModulePath\LICENSE -ItemType File | Out-Null
-        
-        # Convert the $PackageInfo to JSON
-        Write-Debug 'Converting package info to JSON...'
-        $JsonData = ConvertTo-Json -InputObject $PackageInfo
-        
-        # Store all package info to the module's package.json
-        Write-Debug 'Adding package info to package.json...'
-        Out-File -FilePath $PackageJson -InputObject $JsonData
-        
-        # Add the MIT License text to the LICENSE file
-        Write-Debug 'Adding MIT license content to LICENSE file...'
-        Out-File -FilePath $FullModulePath\LICENSE -InputObject $MITLicense
-        
-        Write-Verbose "The $ModuleName module has been successfuly created."
-        
-        # Open the folder containing the module as long as $OpenInVSCode is true
-        if ($OpenInVSCode) {
-            Write-Debug 'Opening the module directory in Visual Studio Code...'
-            code $FullModulePath
+  [CmdletBinding()]
+  [Alias('nodemod', 'new-nodemod', 'newmod', 'newmodule', 'newnpm')]
+  param (
+    [Parameter(Mandatory = $true,
+               Position = 1,
+               HelpMessage = 'The name of the module to create.')]
+    [ValidateScript({
+        if ($_ -match '^(?:@[a-z0-9-~][a-z0-9-._~]*/)?[a-z0-9-~][a-z0-9-._~]*$') {
+          return $true
+        } else {
+          throw "Your module name must match the following pattern: ^(?:@[a-z0-9-~][a-z0-9-._~]*/)?[a-z0-9-~][a-z0-9-._~]*`$`nSee https://docs.npmjs.com/files/package.json#name for more info."
         }
+      })]
+    [Alias('Name', 'Module')]
+    [String]$ModuleName,
+    
+    [Parameter(Mandatory = $false,
+               Position = 2,
+               HelpMessage = 'The path where the module is created.')]
+    [ValidateScript({
+        if (Test-Path $_) {
+          return $true
+        } else {
+          throw 'The provided path does not exist.'
+        }
+      })]
+    [Alias('Path')]
+    [System.IO.FileInfo]$ModulePath = 'D:\Development\Projects\NodeJS_Packages',
+    
+    [Parameter(Mandatory = $false,
+               Position = 3,
+               HelpMessage = 'Do you wish to open this module in VSCode after creation?')]
+    [Alias('Open')]
+    [bool]$OpenInVSCode = $true
+  )
+  
+  $AuthorInfo = Get-AuthorInfo
+  $ScriptsInfo = Get-ScriptsInfo
+  $DevDependencies = Get-DevDependencies
+  $PackageInfo = Get-PackageInfo -ModuleName $ModuleName
+  
+  $FullModulePath = Join-Path -Path $ModulePath -ChildPath $ModuleName
+  $PackageJson = Join-Path -Path $FullModulePath -ChildPath 'package.json'
+  
+  if (Test-Path -Path $FullModulePath) {
+    # There is a folder at the provided path with the same name as the $ModuleName
+    throw 'A folder with the same name exists at the provided path. Please choose a different module name or path.'
+  } else {
+    # Create directory for the module
+    Write-Debug 'Creating module directory...'
+    New-Item -Path $FullModulePath -ItemType Directory | Out-Null
+    
+    # Create the new package.json file
+    Write-Debug 'Creating modules package.json...'
+    New-Item -Path $PackageJson -ItemType File | Out-Null
+    
+    # Create the main index.js for most code logic
+    Write-Debug 'Creating modules index.js...'
+    New-Item -Path $FullModulePath\index.js -ItemType File | Out-Null
+    
+    # Create the LICENSE file that will contain the MIT License
+    Write-Debug 'Creating modules empty LICENSE file...'
+    New-Item -Path $FullModulePath\LICENSE -ItemType File | Out-Null
+    
+    # Convert the $PackageInfo to JSON
+    Write-Debug 'Converting package info to JSON...'
+    $JsonData = ConvertTo-Json -InputObject $PackageInfo
+    
+    # Store all package info to the module's package.json
+    Write-Debug 'Adding package info to package.json...'
+    Out-File -FilePath $PackageJson -InputObject $JsonData
+    
+    # Add the MIT License text to the LICENSE file
+    Write-Debug 'Adding MIT license content to LICENSE file...'
+    Out-File -FilePath $FullModulePath\LICENSE -InputObject $MITLicense
+    
+    Write-Verbose "The $ModuleName module has been successfuly created."
+    
+    # Open the folder containing the module as long as $OpenInVSCode is true
+    if ($OpenInVSCode) {
+      Write-Debug 'Opening the module directory in Visual Studio Code...'
+      code $FullModulePath
     }
+  }
 }
 
 function Get-AuthorInfo() {
-    return @{
-        name = 'Alcha'
-        email = 'alcha@hasslefree.solutions'
-        url = 'https://hasslefree.solutions'
-    }
+  return @{
+    name = 'Alcha'
+    email = 'alcha@hasslefree.solutions'
+    url = 'https://hasslefree.solutions'
+  }
 }
 
 function Get-ScriptsInfo() {
-    return @{
-        start = 'node ./index.js'
-        test = 'jest'
-    }
+  return @{
+    start = 'node ./index.js'
+    test = 'jest'
+  }
 }
 
 function Get-DevDependencies() {
-    return @{
-        jest = ''
-        standard = ''
-    }
+  return @{
+    jest = ''
+    standard = ''
+  }
 }
 
 function Get-PackageInfo() {
-    param (
-        [parameter(Mandatory = $true)]
-        [String]$ModuleName
-    )
-    
-    return @{
-        name = $ModuleName.ToLower()
-        displayName = $ModuleName.Substring(0, 1).ToUpper() + $ModuleName.Substring(1).toLower()
-        version = '0.0.1'
-        description = 'This is a placeholder description to be updated at a later date.'
-        main = 'index.js'
-        license = 'MIT'
-        scripts = $ScriptsInfo
-        author = $AuthorInfo
-        devDependencies = $DevDependencies
-    }
+  param (
+    [parameter(Mandatory = $true)]
+    [String]$ModuleName
+  )
+  
+  return @{
+    name = $ModuleName.ToLower()
+    displayName = $ModuleName.Substring(0, 1).ToUpper() + $ModuleName.Substring(1).toLower()
+    version = '0.0.1'
+    description = 'This is a placeholder description to be updated at a later date.'
+    main = 'index.js'
+    license = 'MIT'
+    scripts = $ScriptsInfo
+    author = $AuthorInfo
+    devDependencies = $DevDependencies
+  }
 }
 
 $MITLicense = @"
