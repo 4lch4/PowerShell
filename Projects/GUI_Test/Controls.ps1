0cb0bbab2295fffb882c409e2e9f67019bf5ebca
Devin Leaman <dleaman212@gmail.com>
2018-05-28 00:07:45 -0500

    Added a GUI Test Project
    Followed a guide linked in the GUi_Test.ps1 file that led to this result, just tinkering with some stuff.

---
+++ Projects/GUI_Test/Controls.ps1
@@ -0,0 +1,43 @@
+﻿#	===========================================================================
+#	Created on:   5/27/2018 @ 23:59
+#	Created by:   Alcha
+#	Organization: HassleFree Solutions, LLC
+#	Filename:     Get-Controls.ps1
+#	===========================================================================
+
+function Get-TextBox () {
+  $TextBox = New-Object -TypeName System.Windows.Forms.TextBox
+  $TextBox.Location = New-Object -TypeName System.Drawing.Point(10, 40)
+  $TextBox.Size = New-Object System.Drawing.Size(260, 20)
+  
+  return $TextBox
+}
+
+function Get-OKButton () {
+  $OKButton = New-Object -TypeName System.Windows.Forms.Button
+  $OKButton.Location = New-Object -TypeName System.Drawing.Point(75, 120)
+  $OKButton.Size = New-Object -TypeName System.Drawing.Size(75, 23)
+  $OKButton.Text = 'OK'
+  $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+  
+  return $OKButton
+}
+
+function Get-CancelButton () {
+  $CancelButton = New-Object -TypeName System.Windows.Forms.Button
+  $CancelButton.Location = New-Object -TypeName System.Drawing.Point(150, 120)
+  $CancelButton.Size = New-Object -TypeName System.Drawing.Size(75, 23)
+  $CancelButton.Text = 'Cancel'
+  $CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+  
+  return $CancelButton
+}
+
+function Get-Label () {
+  $Label = New-Object -TypeName System.Windows.Forms.Label
+  $Label.Location = New-Object -TypeName System.Drawing.Point(10, 20)
+  $Label.Size = New-Object -TypeName System.Drawing.Size(280, 20)
+  $Label.Text = 'Please enter the requested information below.'
+  
+  return $Label
+}
---
+++ Projects/GUI_Test/GUI_Test.ps1
@@ -0,0 +1,51 @@
+﻿# ===========================================================================
+# Created on:   5/27/2018 @ 23:13
+# Created by:   Alcha
+# Organization: HassleFree Solutions, LLC
+# Filename:     GUI_Test.ps1
+# ===========================================================================
+
+<#
+.DESCRIPTION
+	A simple test of some GUI shit based off this article: http://bit.ly/2xkyxTx
+
+.EXAMPLE
+	.\GUI_Test.ps1
+#>
+
+. .\Controls.ps1
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$Form = New-Object -TypeName System.Windows.Forms.Form
+$Form.Text = 'Data Entry Form'
+$Form.Size = New-Object -TypeName System.Drawing.Size(300, 200)
+$Form.StartPosition = 'CenterScreen'
+$Form.Icon = New-Object -TypeName System.Drawing.Icon(".\Assets\HassleFree_Solutions.ico")
+
+$Label = Get-Label
+$OKButton = Get-OKButton
+$CancelButton = Get-CancelButton
+$TextBox = Get-TextBox
+
+$Form.AcceptButton = $OKButton
+$Form.Controls.Add($OKButton)
+
+$Form.CancelButton = $CancelButton
+$Form.Controls.Add($CancelButton)
+
+$Form.Controls.Add($Label)
+$Form.Controls.Add($TextBox)
+
+$Form.TopMost = $true
+
+$Form.add_Shown({$TextBox.Select()})
+
+$Result = $Form.ShowDialog()
+
+if ($Result -eq [System.Windows.Forms.DialogResult]::OK) {
+  Write-Host 'Here is some text for you bastards...'
+  Write-Host $TextBox.Text
+} else {
+  Write-Host 'Script was canceled.'
+}
